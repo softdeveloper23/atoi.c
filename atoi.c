d5ef279ac711d734de76c836f8e6fb1b31621fba
unknown <brannongarrett77@gmail.com>
2023-12-09 15:36:12 +0100

    Updated the code

--- atoi.c
+++ atoi.c
@@ -1,9 +1,10 @@
-#include <cs50.h>
+#include "cs50.h"
 #include <ctype.h>
 #include <math.h>
 #include <stdio.h>
 #include <string.h>
 
+int convert_helper(string input, int index);
 int convert(string input);
 
 int main(void)
@@ -23,10 +24,18 @@ int main(void)
     printf("%i\n", convert(input));
 }
 
+int convert_helper(string input, int index)
+{
+    if (index < 0)
+    {
+        return 0;
+    }
+
+    int digit = input[index] - '0';
+    return digit * pow(10, strlen(input) - 1 - index) + convert_helper(input, index - 1);
+}
+
 int convert(string input)
 {
-    for (int i = strlen(input - 1); i > 0; i--)
-    {
-        // Finish loop
-    }
+    return convert_helper(input, strlen(input) - 1);
 }
